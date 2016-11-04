07c4b22e57680af774a038da947c9ff0316aac03
Matthew Gould <matt@everettgould.com>
2016-11-04 08:56:22 -0700

    wow, had a lot of norm errors there, glad I reviewed.  make sure tabs align after function declarations, no space between function name and parameters.  add a space after control statements like while and if.  do not assign strings when you declare them, put them on a line below.

--- ex00/ft_generic.c
+++ ex00/ft_generic.c
@@ -6,24 +6,25 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 08:25:50 by mgould            #+#    #+#             */
-/*   Updated: 2016/11/04 08:38:31 by mgould           ###   ########.fr       */
+/*   Updated: 2016/11/04 08:54:57 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
 void	ft_generic(void)
 {
-	int i;
-	char *str = "Tu tu tu tu ; Tu tu tu tu\n";
+	int		i;
+	char	*str;
 
+	str = "Tu tu tu tu ; Tu tu tu tu\n";
 	i = 0;
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		ft_putchar(str[i]);
 		i++;
